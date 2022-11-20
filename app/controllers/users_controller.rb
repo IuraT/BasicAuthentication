933a08836ac1145b0017e50dd5a2575183d46a73
Iurie Turcan <iurie.turcan@saltedge.com>
2022-11-20 20:21:03 +0200

    Add User sign_up functionality
    - Instaled the following gems: bcrypt, rails-pry
    - User created on users#create and redirects to users#new

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(valid_params)
+    if @user.save
+      redirect_to root_path, notice: "User created"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def valid_params
+    params.require(:user).permit(:name, :surname, :email, :password, :password_confirmation)
+  end
+end
